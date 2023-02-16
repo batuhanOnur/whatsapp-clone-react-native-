387afba008a5e306b6efb0644183e530dceb9074
batuhanonur <batuhanonur29@gmail.com>
2023-02-16 13:56:12 +0300

    adding keyboard avoiding view

--- src/screens/ChatScreen.js
+++ src/screens/ChatScreen.js
@@ -1,18 +1,26 @@
-import { ImageBackground, StyleSheet, Text, FlatList } from 'react-native'
+import { ImageBackground, StyleSheet, KeyboardAvoidingView, FlatList } from 'react-native'
+import InputBox from '../components/InputBox/Index'
 import Message from '../components/Message'
 import bg from '../assets/images/bg.png'
 import messages from '../assets/data/messages.json'
 
+
 const ChatScreen = () => {
     return (
-        <ImageBackground source={bg} style={styles.bg}>
-            <FlatList 
-                data={messages}
-                renderItem={({ item }) => <Message message={item} />}
-                style={styles.list}
-                inverted
-            />
-        </ImageBackground>
+        <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        style={styles.bg}
+        >
+            <ImageBackground source={bg} style={styles.bg}>
+                <FlatList 
+                    data={messages}
+                    renderItem={({ item }) => <Message message={item} />}
+                    style={styles.list}
+                    inverted
+                />
+                <InputBox />
+            </ImageBackground>
+        </KeyboardAvoidingView>
     )
 }
 
