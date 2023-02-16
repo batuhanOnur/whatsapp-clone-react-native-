d01399c094ec605295f2fd23d1415d4d6cff516a
batuhanonur <batuhanonur29@gmail.com>
2023-02-16 15:33:20 +0300

    remove padding

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import ChatScreen from './src/screens/ChatScreen';
+import Navigator from './src/navigation';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <ChatScreen />
+      <Navigator />
       <StatusBar style="auto" />
     </View>
   );
@@ -16,6 +16,5 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     justifyContent: 'center',
-    paddingVertical: 50,
   },
 });
