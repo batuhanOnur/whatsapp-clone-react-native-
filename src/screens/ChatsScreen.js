522fb26aa2d0801bc4aac9229a3bc9c0e7e735e4
batuhanonur <batuhanonur29@gmail.com>
2023-02-16 10:41:32 +0300

    create project and dummy data

---
+++ src/screens/ChatsScreen.js
@@ -0,0 +1,16 @@
+import { StyleSheet, FlatList, View } from 'react-native'
+import chats from '../assets/data/chats.json'
+import ChatListItem from '../components/ChatListItem'
+
+const ChatsScreen = () => {
+    return (
+        <FlatList 
+            data={chats}
+            renderItem={({item}) => <ChatListItem chat={item} />}
+        />
+    )
+}
+
+export default ChatsScreen
+
+const styles = StyleSheet.create({})
